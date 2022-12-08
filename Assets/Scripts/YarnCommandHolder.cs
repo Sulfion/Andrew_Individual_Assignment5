764ec4dadcce38b1fd905219f771e9324602ea34
Sulfion <2246364@swansea.ac.uk>
2022-12-08 21:40:58 +0000

    created basic framework for checking variable states in YarnScript

--- Assets/Scripts/YarnCommandHolder.cs
+++ Assets/Scripts/YarnCommandHolder.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn;
 using Yarn.Unity;
+using static UnityEngine.Rendering.DebugUI;
 
 public class YarnCommandHolder : MonoBehaviour
 {
-    public InMemoryVariableStorage yarnInMemoryStorage;
+    public InMemoryVariableStorage variableStorage;
 
+    float testVariable;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,14 @@ public class YarnCommandHolder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        VariableTracker();
+    }
 
+    //this method tracks what variables have changed.
+    //For example, if a player accepts a quest, delivers an item to the correct item, the current state will be stored here
+    private void VariableTracker()
+    {
+        variableStorage = GameObject.FindObjectOfType<InMemoryVariableStorage>();
+        variableStorage.TryGetValue("$visited_town_crier", out testVariable);
     }
 }
