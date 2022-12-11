4e6fc1caabf8c9348a2053013d33dc691f6c7a3f
Sulfion <2246364@swansea.ac.uk>
2022-12-11 15:45:03 +0000

    fixed some bugs, such as player starting too high up and falling

--- Assets/Scripts/EndSequenceManager.cs
+++ Assets/Scripts/EndSequenceManager.cs
@@ -11,11 +11,6 @@ public class EndSequenceManager : MonoBehaviour
     private float completeAllQuests = 0;
     private bool startEndSequence = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -23,6 +18,9 @@ public class EndSequenceManager : MonoBehaviour
         VariableTracker();
     }
 
+    //these script checks if all of the quests have been completed in yarn
+    //if that is true, it sets the end sequence start flag to true
+    //then it checks if the player is near the correct NPC and completed all the quests, which lets you move onto the final dialogue
     private void OnTriggerEnter(Collider other)
     {
         if (this.gameObject.CompareTag("NPC1") && completeAllQuests == 1)
